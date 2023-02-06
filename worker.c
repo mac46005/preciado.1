952c73f99f6a1bf20ab1f6abf115bf010cebe18b
marco preciado <preciado@hoare7.cs.umsl.edu>
2023-02-06 17:00:19 -0600

    worker created and tested

---
+++ worker.c
@@ -0,0 +1,15 @@
+//AUTHOR: Marco Preciado
+//Description: This is the worker.c file
+//This program simply uses the supplied integers and loops that amount of times
+//printing the PID of the child and parent.
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char ** argv){
+	int loopCount = atoi(argv[1]);
+	int i;
+	for(i = 0; i < loopCount; i++){
+		printf("WORKER PID: %d, ", getpid());
+		printf("PPID: %d ", getppid());
+		printf("Iteration: %d\n", i + 1);
+	}
+}
